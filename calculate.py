9450497f8ec3737847f68b233cfb2e21078fec96
mphokholobeng <mphokholobeng@gmail.com>
2025-07-10 18:31:27 +0200

    week9 lessons

---
+++ calculate.py
@@ -0,0 +1,9 @@
+
+def area(length, width):
+    # Function to calculate the area
+    return length * width
+
+
+def perimeter(length, width):
+    # Function to calculate the perimeter
+    return length + width
